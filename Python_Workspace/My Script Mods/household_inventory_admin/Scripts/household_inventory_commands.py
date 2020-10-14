3e2a41d59701c7c0d646431edd0059c4d96defa3
irql <interruptrequestlevel@gmail.com>
2020-10-13 19:29:15 -0700

    Fixed bug where the amount to subtract from household funds by is under-calculated

--- Python_Workspace/My Script Mods/household_inventory_admin/Scripts/household_inventory_commands.py
+++ Python_Workspace/My Script Mods/household_inventory_admin/Scripts/household_inventory_commands.py
@@ -1,8 +1,7 @@
-import sims4.commands
-import services
 import build_buy
 import objects
-
+import services
+import sims4.commands
 from indexed_manager import ObjectIDError
 from objects.object_enums import ItemLocation
 from protocolbuffers import Consts_pb2
@@ -11,7 +10,9 @@ from protocolbuffers import Consts_pb2
 @sims4.commands.Command('irql.debug', command_type=sims4.commands.CommandType.Live)
 def debug(_connection=None):
     output = sims4.commands.CheatOutput(_connection)
-    output('Current sim inventory items: {}'.format(len(services.get_active_sim().inventory_component)))
+    active_sim_info = services.active_sim_info()
+    output("Items in {} {}'s inventory: {}".format(active_sim_info.first_name, active_sim_info.last_name,
+                                                   len(services.get_active_sim().inventory_component)))
     output('Items in household inventory: {}'.format(
         len(build_buy.get_object_ids_in_household_inventory(services.active_household_id()))))
 
@@ -21,8 +22,10 @@ def move_sim_inventory_to_household(_connection=None):
     output = sims4.commands.CheatOutput(_connection)
     active_sim = services.get_active_sim()
     active_sim_info = services.active_sim_info()
-    output("Moving all items in {} {}'s inventory to household inventory.".format(active_sim_info.first_name, active_sim_info.last_name))
+    output("Moving all items in {} {}'s inventory to household inventory.".format(active_sim_info.first_name,
+                                                                                  active_sim_info.last_name))
     active_sim.inventory_component.push_items_to_household_inventory()
+
 
 @sims4.commands.Command('irql.move_household_inventory_to_sim', command_type=sims4.commands.CommandType.Live)
 def move_household_inventory_to_sim(_connection=None):
@@ -36,8 +39,21 @@ def move_household_inventory_to_sim(_connection=None):
         output(
             "Moving {} items from household into {} {}'s inventory.".format(len(object_ids), active_sim_info.first_name,
                                                                             active_sim_info.last_name))
+        total_cost = 0
         for id in object_ids:
-            _move_item_from_household_to_sims_inventory(output, id, object_manager, active_sim, household)
+            _, cost = _move_item_from_household_to_sims_inventory(output, id, object_manager, active_sim, household)
+            total_cost += cost
+        # build_buy.remove_object_from_household_inventory() has the side-effect of bumping up
+        # the current household funds as if we are performing a sale, when in reality we merely
+        # want to move the object.
+        #
+        # Fortunately, if we immediately deduct the same value from the household funds, then
+        # the UI doesn't even have a chance to show that anything changed.
+        #
+        if not household.funds.try_remove(total_cost,
+                                          Consts_pb2.TELEMETRY_HOUSEHOLD_TRANSFER_LOSS, sim=active_sim,
+                                          require_full_amount=False):
+            output('WARN: failed to subtract final amount {}'.format(total_cost))
     else:
         output("No items found in household inventory.")
 
@@ -46,35 +62,27 @@ def _move_item_from_household_to_sims_inventory(output, object_id, object_manage
     object = _copy_object(output, object_id, household.id)
     if object is None:
         output('Failed to copy object')
-        return False
+        return False, 0
     inventory = active_sim.inventory_component
     if inventory.can_add(object):
         # We need to make a copy of the original object prior to moving it into the sims' inventory
         #
         if inventory.player_try_add_object(object):
-            if build_buy.object_exists_in_household_inventory(object_id, household.id):
-                if not build_buy.remove_object_from_household_inventory(object_id, household):
-                    output('FAILED: remove_object_from_household_inventory')
-                    return False  # don't destroy object if it was successfully added to sim inventory
-                else:
-                    # build_buy.remove_object_from_household_inventory() has the side-effect of bumping up
-                    # the current household funds as if we are performing a sale, when in reality we merely
-                    # want to move the object.
-                    #
-                    # Fortunately, if we immediately deduct the same value from the household funds, then
-                    # the UI doesn't even have a change to show that anything changed.
-                    #
-                    if not household.funds.try_remove(object.current_value,
-                                                      Consts_pb2.TELEMETRY_HOUSEHOLD_TRANSFER_LOSS, sim=active_sim,
-                                                      require_full_amount=False):
-                        output('WARN: failed to subtract amount {}'.format(object.current_value))
-                    return True
+            # We need to calculate how much money "remove_object_from_household_inventory" will
+            # give us for removing this object so that we can subtract this amount later.
+            #
+            original_household_funds = household.funds.money
+            if not build_buy.remove_object_from_household_inventory(object_id, household):
+                output('FAILED: remove_object_from_household_inventory')
+                return False, 0  # don't destroy object if it was successfully added to sim inventory
+            else:
+                return True, household.funds.money - original_household_funds
         else:
             output('FAILED: player_try_add_object')
     # Destroy the copied object if we can't add it to sim inventory
     #
     object.destroy(cause='Removing orphaned object that cant be added to sim inventory')
-    return False
+    return False, 0
 
 
 def _copy_object(output, object_id, household_id):
